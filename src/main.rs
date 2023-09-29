2033c3d1ea6174a3bf322ee79b94a9d5433486e5
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2023-09-29 17:59:17 -0400

    load templates

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,46 @@
 use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
 use image::{Rgb, RgbImage};
 use rand::Rng;
+use std::collections::HashMap;
 use std::io::{Cursor, Seek};
+use serde::{Deserialize, Serialize};
+use serde_json;
+use glob::glob;
+
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct Template {
+    template_name: String,
+    //image: RgbImage,
+    text_fields: Vec<TextField>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct TextField {
+    font: String,
+    color: String,
+    size: u32,
+    default_text: String,
+    bound_x_left: u32,
+    bound_x_right: u32,
+    bound_y_bottom: u32,
+    bound_y_top: u32,
+}
+
+fn load_templates() -> HashMap<String, Template> {
+    let templates: HashMap<String, Template> = glob("templates/*.json")
+        .expect("Failed to read glob pattern")
+        .filter_map(|entry| entry.ok())
+        .map(|file_path| {
+            let json_content = std::fs::read_to_string(&file_path).expect("Failed to read file");
+            let template: Template = serde_json::from_str(&json_content).expect("Failed to deserialize JSON");
+            (template.template_name.clone(), template)
+        })
+        .collect();
+
+    println!("Loaded {} templates", templates.len());
+    templates
+}
 
 fn generate_random_noise_image() -> RgbImage {
     let mut rng = rand::thread_rng();
@@ -50,16 +89,34 @@ async fn template_test() -> impl Responder {
 }
 
 #[get("/{template_name}")]
-async fn template_default(template_name: web::Path<String>) -> impl Responder {
+async fn template_default(
+    template_name: web::Path<String>,
+    templates: web::Data<HashMap<String, Template>>,
+) -> impl Responder {
+    
     let template_name = template_name.to_string();
-    template_name
+    match templates.get(&template_name) {
+        Some(template) => {
+            // Do something with the template
+            // For example, return a response with the template data
+            HttpResponse::Ok().json(template)
+        }
+        None => {
+            // Handle the case when the template is not found
+            HttpResponse::NotFound().finish()
+        }
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    // Load templates once during application setup
+    let templates = load_templates();
+
+    HttpServer::new(move || {
         App::new()
-            // earlier entries get priority
+            // Configure state with the loaded templates
+            .app_data(web::Data::new(templates.clone()))
             .service(index)
             .service(template_test)
             .service(template_default)
